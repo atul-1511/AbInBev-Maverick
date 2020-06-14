9a67d01449794ae7e915b94ca4558cd5987208ef
Atul Kumar <atul.iitkharagpur@gmail.com>
2020-06-15 02:49:32 +0530

    Update CBIR_Beer.py

--- CBIR_Beer.py
+++ CBIR_Beer.py
@@ -69,13 +69,13 @@ class ColorDescriptor:
     
 
 # initialize the color descriptor
-cd = ColorDescriptor((8, 12, 3))
+cd = ColorDescriptor((20, 20, 20))
 
 # open the output index file for writing
 output = open(r'C:\Users\Atul Kumar\Downloads\index.csv', "w")
 
 # use glob to grab the image paths and loop over them
-for i in range(1,21):
+for i in range(1,20):
     image = cv2.imread(r'C:\Users\Atul Kumar\Downloads\Problem Statement 2\Beer\\' + str(i) + '.jpg')
     # describe the image
     features = cd.describe(image)
@@ -86,3 +86,84 @@ for i in range(1,21):
 
 # close the index file
 output.close()
+
+# import the necessary packages
+import csv
+
+class Searcher:
+    def __init__(self, indexPath):
+        # store our index path
+        self.indexPath = indexPath
+
+    def search(self, queryFeatures, limit = 19):
+        # initialize our dictionary of results
+        results = {}
+
+        # open the index file for reading
+        with open(self.indexPath) as f:
+            # initialize the CSV reader
+            reader = csv.reader(f)
+
+            # loop over the rows in the index
+            for row in reader:
+                # parse out the image ID and features, then compute the
+                # chi-squared distance between the features in our index
+                # and our query features
+                features = [float(x) for x in row[1:]]
+                d = self.chi2_distance(features, queryFeatures)
+
+                # now that we have the distance between the two feature
+                # vectors, we can udpate the results dictionary -- the
+                # key is the current image ID in the index and the
+                # value is the distance we just computed, representing
+                # how 'similar' the image in the index is to our query
+                results[row[0]] = d
+
+            # close the reader
+            f.close()
+
+        # sort our results, so that the smaller distances (i.e. the
+        # more relevant images are at the front of the list)
+        results = sorted([(v, k) for (k, v) in results.items()])
+
+        # return our (limited) results
+        return results[:limit]
+
+    def chi2_distance(self, histA, histB, eps = 1e-10):
+        # compute the chi-squared distance
+        d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+            for (a, b) in zip(histA, histB)])
+
+        # return the chi-squared distance
+        return d
+    
+    
+import pandas as pd
+Index_DF = pd.read_csv(r'C:\Users\Atul Kumar\Downloads\index.csv', header=None)
+
+
+
+
+# initialize the image descriptor
+cd = ColorDescriptor((20, 20 , 20))
+
+# load the query image and describe it
+query = cv2.imread(r'C:\Users\Atul Kumar\Downloads\Beer Bottles\20.png')
+features = cd.describe(query)
+
+# perform the search
+searcher = Searcher(r'C:\Users\Atul Kumar\Downloads\index.csv')
+results = searcher.search(features)
+
+# display the query
+from matplotlib import pyplot
+pyplot.imshow(query)
+
+# loop over the results
+for (score, resultID) in results:
+    # load the result image and display it
+    print(resultID)
+    result = cv2.imread(r'C:\Users\Atul Kumar\Downloads\Problem Statement 2\Beer\\' + resultID + '.jpg')
+    pyplot.imshow(result)
+    
+    
